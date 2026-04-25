eee4fd12a8d8fc33d3fe2fe92fdc387108e5a144
aqnya <siyueba73@gmail.com>
2026-04-26 02:47:23 +0800

    add zig.

--- lib/info.dart
+++ lib/info.dart
@@ -4,15 +4,18 @@ import 'package:ffi/ffi.dart';
 final _lib = DynamicLibrary.open('libncore.so');
 
 final _getKernelRelease = _lib
-    .lookup<NativeFunction<Int32 Function(Pointer<Utf8>, Size)>>('get_kernel_release')
+    .lookup<NativeFunction<Int32 Function(Pointer<Utf8>, Size)>>(
+      'get_kernel_release',
+    )
     .asFunction<int Function(Pointer<Utf8>, int)>();
 
 String getKernelRelease() {
-  final buf = calloc<Utf8>(256);
+  final buf = calloc<Uint8>(256);
   try {
-    final n = _getKernelRelease(buf, 256);
+    final n = _getKernelRelease(buf.cast<Utf8>(), 256);
     if (n < 0) return 'uname failed';
-    return buf.toDartString(length: n);
+
+    return buf.cast<Utf8>().toDartString(length: n);
   } finally {
     calloc.free(buf);
   }
