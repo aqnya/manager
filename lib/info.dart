f97165a21c2e3899dfcc3f5d6d76c3de37f863de
aqnya <siyueba73@gmail.com>
2026-04-25 22:52:40 +0800

    test add zig.

--- lib/info.dart
+++ lib/info.dart
@@ -1,43 +1,15 @@
-import 'dart:ffi';
 import 'dart:io';
-import 'package:ffi/ffi.dart';
 
-final DynamicLibrary _libc =
-    Platform.isAndroid ? DynamicLibrary.open('libc.so') : DynamicLibrary.process();
-
-typedef NativeUname = Int32 Function(Pointer<Utsname>);
-typedef DartUname = int Function(Pointer<Utsname>);
-
-final DartUname _uname =
-    _libc.lookupFunction<NativeUname, DartUname>('uname');
-
-final class Utsname extends Struct {
-  @Array(65)
-  external Array<Int8> sysname;
-
-  @Array(65)
-  external Array<Int8> nodename;
-
-  @Array(65)
-  external Array<Int8> release;
-
-  @Array(65)
-  external Array<Int8> version;
-
-  @Array(65)
-  external Array<Int8> machine;
-}
-
-String getKernelRelease() {
-  final uts = calloc<Utsname>();
-
+Future<String> getKernelReleaseByCmd() async {
   try {
-    if (_uname(uts) != 0) return "uname failed";
+    final result = await Process.run('/system/bin/uname', ['-r']);
 
-    return uts.ref.release
-        .cast<Int8>()
-        .toDartString();
-  } finally {
-    calloc.free(uts);
+    if (result.exitCode == 0) {
+      return (result.stdout as String).trim();
+    } else {
+      return "uname failed: ${result.stderr}";
+    }
+  } catch (e) {
+    return "error: $e";
   }
 }
