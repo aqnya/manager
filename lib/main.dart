83e68c9eaa847fd6a6a06a2c7b480bf0fcea6420
aqnya <siyueba73@gmail.com>
2026-04-25 20:07:46 +0800

    remove action.

--- lib/main.dart
+++ lib/main.dart
@@ -49,6 +49,7 @@ class _MainPageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) {
+  final colorScheme = Theme.of(context).colorScheme;
     return Scaffold(
       backgroundColor: colorScheme.surface, 
       extendBody: true,
