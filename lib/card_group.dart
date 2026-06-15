5fb1a9b1774c6b6b485a1bee62a56109c87f4bd2
aqnya <xiaweilan56@gmail.com>
2026-06-16 00:25:35 +0800

    fix.

--- lib/card_group.dart
+++ lib/card_group.dart
@@ -21,7 +21,7 @@ class CardGroup extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final color = Theme.of(context).colorScheme.surfaceVariant;
+    final color = Theme.of(context).colorScheme.onSurfaceVariant;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Column(
